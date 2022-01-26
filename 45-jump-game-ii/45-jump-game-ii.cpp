160f1878b8bed4dffceab099a398ce2c894fd54e
Pavitra <pavi.8081@gmail.com>
2022-01-26 09:57:49 +0530

    Time: 25 ms (56.18%), Space: 16.6 MB (39.98%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        const int len = nums.size();
-        vector<int> min(len, INT_MAX);
-
-        min[0] = 0;
+        vector<int>::size_type len = nums.size();
+        if(len <= 0) return 0;
+        int max = 0, current_max = 0, mjump = 0;
         for (int i = 0; i < len; i++) {
-            for (int k = i; k <= i + nums[i] && k < len; k++) {
-                min[k] = std::min(min[k], min[i] + 1);
+            if(i > current_max){
+                mjump++;
+                current_max = max;
             }
+            max = std::max(max, i + nums[i]);
         }
-        return min[len - 1];
+        return mjump;
     }
 };
