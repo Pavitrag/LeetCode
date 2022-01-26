2176ea10d6dab1b01390cfe65c917fa4a5bbec96
Pavitra <pavi.8081@gmail.com>
2022-01-26 10:01:43 +0530

    Time: 16 ms (75.96%), Space: 16.6 MB (39.98%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -3,11 +3,11 @@ public:
     int jump(vector<int>& nums) {
         vector<int>::size_type len = nums.size();
         if(len <= 0) return 0;
-        int max = 0, current_max = 0, mjump = 0;
+        int max = 0, farthestWithCurrentJump = 0, mjump = 0;
         for (int i = 0; i < len; i++) {
-            if(i > current_max){
+            if(i > farthestWithCurrentJump){
                 mjump++;
-                current_max = max;
+                farthestWithCurrentJump = max;
             }
             max = std::max(max, i + nums[i]);
         }
