d410a3a464131ac392a17212d8f7b5a5d0fe3f31
Pavitra <pavi.8081@gmail.com>
2022-01-30 15:13:30 +0530

    Time: 1 ms (59.48%), Space: 43.6 MB (5.38%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,19 +1,44 @@
 class Solution {
-    private double powerLog(double x, long y) {
-        if (y == 0)
-            return 1;
-        if (y % 2 == 0) {
-            double ret = powerLog(x, y / 2);
-            return ret*ret;
+    // private double powerLog(double x, long y) {
+    //     if (y == 0)
+    //         return 1;
+    //     if (y % 2 == 0) {
+    //         double ret = powerLog(x, y / 2);
+    //         return ret*ret;
+    //     }
+    //     return powerLog(x, y - 1)*x;
+    // }
+    
+    private double powerLogIterative(double x, long y) {
+        long prev = 1;
+        long itr = 1;
+        if (y == 0) return 1;
+        double accumulated = 1;
+        double curr = 1;
+        while (itr <= y) {
+            prev = itr;
+            if (itr % 2 == 0) {
+                curr *= curr;
+                itr *= 2;
+            } else {
+                curr *= x;
+                itr++;
+            }
+            if(itr > y){
+                accumulated *= curr;
+                y -= prev;
+                itr = 1;
+                curr = 1;
+            }
         }
-        return powerLog(x, y - 1)*x;
+        return accumulated;
     }
     
     public double myPow(double x, int n) {
         long ln = n;
         if(n < 0){
-            return 1.0/powerLog(x, -ln);
+            return 1.0/powerLogIterative(x, -ln);
         }
-        return powerLog(x, ln);    
+        return powerLogIterative(x, ln);    
     }
 }
