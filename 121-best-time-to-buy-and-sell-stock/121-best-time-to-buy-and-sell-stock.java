59f15d0d193ef3abd1d5afeb0f7ef33d855f534a
Pavitra <pavi.8081@gmail.com>
2022-02-01 13:22:17 +0530

    Time: 5 ms (13.31%), Space: 83.8 MB (54.50%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,16 +1,33 @@
 class Solution {
+    //FIRST TRY
+//     public int maxProfit(int[] prices) {
+//         int len = prices.length;
+//         int[] m = new int[len];
+        
+//         for(int i = len - 2; i >= 0; i--){
+//             m[i] = Math.max(prices[i+1], i+1 == len-1 ? Integer.MIN_VALUE : m[i+1]);
+//         }
+        
+//         int maxProfit = 0;
+//         for(int i = 0; i < len-1; i++){
+//             maxProfit = Math.max(maxProfit, m[i] - prices[i]);
+//         }
+//         return maxProfit;
+//     }
+    
+    //SECOND TRY
     public int maxProfit(int[] prices) {
         int len = prices.length;
-        int[] m = new int[len];
+        // int[] m = new int[len];
         
-        for(int i = len - 2; i >= 0; i--){
-            m[i] = Math.max(prices[i+1], i+1 == len-1 ? Integer.MIN_VALUE : m[i+1]);
+        int min = prices[0];
+        int max = 0;
+        
+        for(int i = 1; i < len; i++){
+            min = Math.min(min, prices[i-1]);
+            max = Math.max(max, prices[i] - min);
         }
         
-        int maxProfit = 0;
-        for(int i = 0; i < len-1; i++){
-            maxProfit = Math.max(maxProfit, m[i] - prices[i]);
-        }
-        return maxProfit;
+        return max;
     }
 }
