f0a57ef8b432176f643d017b24dff12b8f962132
Pavitra <pavi.8081@gmail.com>
2022-02-02 00:24:06 +0530

    Time: 244 ms (6.37%), Space: 117.7 MB (5.15%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -12,7 +12,10 @@ class Solution {
         return p[0]*p[0] + p[1]*p[1];    
     }
     
-    private int partition(int[][] points, int begin, int end){        
+    private int partition(int[][] points, int begin, int end){  
+        if(begin > end){
+            return -1;
+        }
         int[] pivot = points[end];
         int dist = distance(pivot);
         for(int i = begin; i < end; i++){
@@ -24,46 +27,21 @@ class Solution {
         return begin;
     }
     
-    private int get(int[][] points, int begin, int end, int k){        
-        int pivot = partition(points, begin, end);
-        if(pivot + 1 < k){
-            return get(points, pivot + 1, end, k);
+    private void partialSort(int[][] points, int begin, int end, int k) {
+        int pivot = end + 1;
+        while (pivot != k) {
+          pivot = partition(points, begin, end);
+          if (pivot + 1 < k) {
+            begin = pivot + 1;
+          } else if (pivot + 1 > k) {
+            end = pivot - 1;
+          }
         }
-        if(pivot + 1 > k){
-            return get(points, begin, pivot-1, k);
-        }
-        return pivot;
-    }
-    
+      }
+
     public int[][] kClosest(int[][] points, int k) {
-        Arrays.sort(points, (s, t) -> distance(s) - distance(t));
-        int[][] kclose = new int[k][];
-        for(int i = 1; i <= k; i++){
-            kclose[i-1] = points[i-1];
-        }
+        partialSort(points, 0, points.length - 1, k);
+        var kclose = Arrays.copyOf(points, k);
         return kclose;
     }
-    
-    
-    
-//     private int[][] get(int[][] points, int k){
-
-//         PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((e1, e2) -> {
-//           int x1 = e1.getKey(), y1 = e1.getValue();
-//           int x2 = e2.getKey(), y2 = e2.getValue();
-//           return (x1 * x1 + y1 * y1) - (x2 * x2 + y2 * y2);
-//         });
-
-//         for (int[] point : points) {
-//           pq.add(new AbstractMap.SimpleEntry<Integer, Integer>(point[0], point[1]));
-//         }
-
-//         int[][] closestk = new int[k][2];
-//         for (int i = 0; i < k; i++) {
-//             Map.Entry<Integer, Integer> point = pq.poll();
-//             closestk[i][0] = point.getKey();
-//             closestk[i][1] = point.getValue();
-//         }
-//         return closestk;
-//     }
 }
