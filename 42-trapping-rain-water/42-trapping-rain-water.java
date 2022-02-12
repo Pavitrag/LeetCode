8dd076f227f0d0087314e4d100a38863fa671be8
Pavitra <pavi.8081@gmail.com>
2022-02-12 19:15:21 +0530

    Time: 1 ms (92.78%), Space: 47.5 MB (5.04%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,22 +1,41 @@
 class Solution {
+//     public int trap(int[] height) {
+//         int len = height.length;
+//         int[] left = new int[len];
+//         int[] right = new int[len];
+        
+//         for(int i = 1; i < len; i++){
+//             left[i] = Math.max(left[i-1], height[i-1]);
+//         }
+        
+//         for(int i = len-2; i>=0; i--){
+//             right[i] = Math.max(right[i + 1], height[i+1]);
+//         }
+        
+//         int w = 0;
+//         for(int i = 0; i< len; i++){
+//             int c = Math.min(left[i], right[i]) - height[i];
+//             if(c > 0){
+//                 w +=  c;
+//             }
+//         }
+//         return w;
+//     }
     public int trap(int[] height) {
         int len = height.length;
         int[] left = new int[len];
-        int[] right = new int[len];
-        
+
         for(int i = 1; i < len; i++){
             left[i] = Math.max(left[i-1], height[i-1]);
         }
-        
-        for(int i = len-2; i>=0; i--){
-            right[i] = Math.max(right[i + 1], height[i+1]);
-        }
-        
+
         int w = 0;
-        for(int i = 0; i< len; i++){
-            int c = Math.min(left[i], right[i]) - height[i];
+        int max = 0;
+        for(int i = len-2; i>=0; i--){
+            max = Math.max(max, height[i+1]);
+            int c = Math.min(left[i], max) - height[i];
             if(c > 0){
-                w +=  c;
+                w += c;
             }
         }
         return w;
