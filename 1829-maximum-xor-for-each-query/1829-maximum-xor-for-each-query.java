77135307a9008d4af8e9117758b8759c09ea8b2c
Pavitra <pavi.8081@gmail.com>
2022-02-27 09:31:23 +0530

    Time: 4 ms (75.53%), Space: 149.3 MB (9.97%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.java
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.java
@@ -1,16 +1,4 @@
 class Solution {
-    private int power(int maxbit){
-        int res = 1;
-        int base = 2;    
-        while(maxbit > 0){
-            if(maxbit % 2 == 1){
-                res *= base;
-            }
-            base *= base;
-            maxbit /=2;
-        }
-        return res - 1;
-    }
     
     public int[] getMaximumXor(int[] nums, int maximumBit) {
         int len = nums.length;
