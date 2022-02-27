c273e695d3519b7de43c04fc046a0860a4016881
Pavitra <pavi.8081@gmail.com>
2022-02-27 09:29:58 +0530

    Time: 4 ms (75.53%), Space: 129.6 MB (64.96%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.java
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.java
@@ -12,18 +12,30 @@ class Solution {
         return res - 1;
     }
     
+//     public int[] getMaximumXor(int[] nums, int maximumBit) {
+//         int len = nums.length;
+//         int[] ret = new int[len];
+//         int xor = 0;
+//         int k = power(maximumBit);
+//         for(int e: nums){
+//             xor ^= e;
+//         }
+        
+//         for(int i = len-1; i >= 0; i--){
+//             ret[len-1 - i] = k^xor;
+//             xor ^= nums[i];
+//         }
+//         return ret;
+//     }
+    
     public int[] getMaximumXor(int[] nums, int maximumBit) {
         int len = nums.length;
         int[] ret = new int[len];
-        int xor = 0;
-        int k = power(maximumBit);
-        for(int e: nums){
-            xor ^= e;
-        }
+        int k = (1 << maximumBit) - 1;
         
-        for(int i = len-1; i >= 0; i--){
-            ret[len-1 - i] = k^xor;
-            xor ^= nums[i];
+        ret[len-1] = k^nums[0];
+        for(int i = len-2; i >= 0; i--){
+            ret[i] = nums[len-1 - i]^ret[i+1];
         }
         return ret;
     }
