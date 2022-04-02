de069ba3119c74727f898f7042558d2ff03c2af9
Pavitra <pavi.8081@gmail.com>
2022-04-03 02:02:22 +0530

    Time: 2 ms (78.87%), Space: 42.2 MB (7.54%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,20 +1,21 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x <= 1)
-          return x;
-        double xx = x;
-        double lo = 0.0, hi = x;
-        while (lo < hi) {
-          double mid = lo + (hi - lo) / 2;
-          if ((long) xx == (long) (mid * mid)) {
-            return (int) mid;
-          }
-          if (x < mid * mid) {
-            hi = mid;
-          } else {
-            lo = mid;
-          }
+        if(x == 1) return x;
+        double lo = 0;
+        double hi = x;
+
+        double precision = 0.00001;
+        double mid= 0.0;
+        while(lo < hi) {
+            mid = lo + (hi - lo)/2;
+            if(Math.abs(mid * mid - x) < precision) {
+                return (int)mid;
+            } else if(mid * mid < x) {
+                lo = mid;
+            } else {
+                hi = mid;
+            }
         }
-        return (int) lo;
+        return (int)mid;
       }
 }
