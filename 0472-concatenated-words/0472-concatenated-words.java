80b33fbf4969f41503d26c8c98610ee3517bc0fe
Pavitra <pavi.8081@gmail.com>
2022-12-26 10:15:12 +0530

    Time: 77 ms (81.72%), Space: 49.4 MB (81.41%) - LeetHub

---
+++ 0472-concatenated-words/0472-concatenated-words.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        Set<String> dict = new HashSet<>();
+
+        for (String word : words) {
+            dict.add(word);
+        }
+        List<String> answer = new ArrayList<>();
+        for (String word : words) {
+            dict.remove(word);
+            if(test(dict, word)){
+                answer.add(word);
+            }
+            dict.add(word);
+
+        }
+        return answer;
+    }
+    private boolean test(Set<String> set, String word){
+        //ensure word is removed from the set before continuing
+        if(set.contains(word)){
+            return true;
+        }
+        for(int i = 1; i < word.length(); i++){
+            String prefix = word.substring(0, i);
+            String suffix = word.substring(i, word.length());
+            if(set.contains(prefix) && (set.contains(suffix) || test(set, suffix))){
+                set.add(suffix);
+                return true;
+            }
+        }
+        return false;
+    }
+}
