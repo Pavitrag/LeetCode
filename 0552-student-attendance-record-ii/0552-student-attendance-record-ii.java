e8821e18bcec13be6ce78707b106cf2d271defd5
Pavitra <pavi.8081@gmail.com>
2023-01-06 12:02:21 +0530

    Time: 193 ms (53.62%), Space: 51.2 MB (72.12%) - LeetHub

---
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    final int MODULO = 1000000007;
+    
+    public int checkRecord(int n) {
+
+            int A = 0, L = 1, P = 2;
+            long[][] dp = new long[n][3];
+
+            dp[0][L] = 1;
+            dp[0][P] = 1;
+
+            for(int i = 1; i < n; i++){
+                dp[i][L] = ((i - 2 < 0 ? 1 : dp[i-2][P]) + dp[i-1][P])%MODULO;
+                dp[i][P] = (dp[i-1][L] + dp[i-1][P])%MODULO;
+            }
+            long count = 0;
+            for(int i = 0; i < n; i++){ // A is on [0, n)
+                count =  (count + ((i-1 < 0 ? 1 : dp[i-1][L] + dp[i-1][P])%MODULO * (n-i-2 < 0 ? 1 : dp[n-i-2][L] + dp[n-i-2][P])%MODULO)%MODULO)%MODULO;
+            }
+
+            //System.out.printf("%d, %d, %d%n", count%MODULO, dp[n-1][P], dp[n-1][L]);
+            return (int)(((count + dp[n-1][P])%MODULO + dp[n-1][L])%MODULO);
+        }
+}
