04dcf4886459f24b445f44ef2245d0b9e8a19fc5
Pavitra <pavi.8081@gmail.com>
2023-01-15 08:48:37 +0530

    Time: 43 ms (86.68%), Space: 39.4 MB (96.44%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.java
+++ 0935-knight-dialer/0935-knight-dialer.java
@@ -1,35 +1,32 @@
 class Solution {
     public int knightDialer(int n) {
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        for(int i = 0; i <= 9; i++){
-            switch(i){
-                case 0 -> map.put(0, List.of(4, 6));
-                case 1 -> map.put(1, List.of(6, 8));
-                case 2 -> map.put(2, List.of(7, 9));
-                case 3 -> map.put(3, List.of(4, 8));
-                case 4 -> map.put(4, List.of(0, 3, 9));
-                case 5 -> map.put(5, List.of());
-                case 6 -> map.put(6, List.of(0, 1, 7));
-                case 7 -> map.put(7, List.of(2, 6));
-                case 8 -> map.put(8, List.of(1, 3));
-                case 9 -> map.put(9, List.of(2, 4));
-            }
-        }
-
         final int MOD = 1_000_000_007;
-        long count = 0;
-        long[][] dp = new long[n+1][10];
-        IntStream.range(0, 10).forEach(r -> dp[0][r]++);
 
-        for(int i = 1; i <n; i++){
-            for(int p = 0; p <= 9; p++){
-                for(int k: map.get(p)) {
-                    dp[i][p] = (dp[i][p] + dp[i - 1][k]) % MOD;
+        long[] curr = new long[10];
+        long[] next = new long[10];
+        Arrays.fill(curr, 1);
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j <= 9; j++){
+                switch(j){
+                    case 0 -> next[0] = (curr[4] + curr[6]) % MOD;
+                    case 1 -> next[1] = (curr[6] + curr[8]) % MOD;
+                    case 2 -> next[2] = (curr[7] + curr[9]) % MOD;
+                    case 3 -> next[3] = (curr[4] + curr[8]) % MOD;
+                    case 4 -> next[4] = (curr[0] + (curr[3] + curr[9]) % MOD) % MOD;
+                    case 5 -> next[5] = 0;
+                    case 6 -> next[6] = (curr[0] + (curr[1] + curr[7]) % MOD) % MOD;
+                    case 7 -> next[7] = (curr[2] + curr[6]) % MOD;
+                    case 8 -> next[8] = (curr[1] + curr[3]) % MOD;
+                    case 9 -> next[9] = (curr[2] + curr[4]) % MOD;
                 }
             }
+            long[] temp = curr;
+            curr = next;
+            next = temp;
         }
+        long count = 0;
         for(int i = 0; i <= 9; i++){
-            count =  (count + dp[n-1][i]) % MOD;
+            count =  (count + curr[i]) % MOD;
         }
         return (int)count;
     }
