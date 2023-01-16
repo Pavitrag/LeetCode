672d5287ccffe2452d61a12a0acecce73c05ce32
Pavitra <pavi.8081@gmail.com>
2023-01-16 22:54:33 +0530

    Time: 1 ms (93.00%), Space: 40.5 MB (84.05%) - LeetHub

--- 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.java
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.java
@@ -1,39 +1,17 @@
 class Solution {
     public String removeDigit(String number, char digit) {
-        int len = number.length();
-        int count = 0, l = -1, r = -1;
-        for(int i = 0; i < number.length(); i++){
+        int len = number.length(), removeIndex = -1;
+
+        for (int i = 0; i < len; i++) {
             char c = number.charAt(i);
-            if(c == digit){
-                l = i;
-                break;
-            }
-        }
-        for(int j = number.length()-1; j >= 0; j--){
-            char c = number.charAt(j);
-            if(c == digit){
-                r = j;
-                break;
-            }
-        }
-        
-        if(l == r){
-            return number.substring(0, l) + (l == len - 1 ? "" : number.substring(l + 1, len));
-        }
-        
-        String res = null;
-        for(int i = 0; i < number.length(); i++){
-            char c = number.charAt(i);
-            if(c == digit){
-                if(i == r){
-                   return number.substring(0, i) + (i == number.length()-1 ? "" : number.substring(i+1, number.length()));
-                }
-                if(i+1 < number.length() && number.charAt(i+1) > c){
-                    return number.substring(0, i) + (i == number.length()-1 ? "" : number.substring(i+1, number.length()));
+            if (c == digit) {
+                removeIndex = i;
+                if(i < len-1 && number.charAt(i+1) > c){
+                    break;
                 }
             }
         }
-        return res;
+        return number.substring(0, removeIndex) + (removeIndex == len - 1 ? "" : number.substring(removeIndex + 1));
         
     }
 }
