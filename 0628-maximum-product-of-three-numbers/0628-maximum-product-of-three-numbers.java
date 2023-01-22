fa03980fe96cf2ac12d2ff3ad3ef03615529eeac
Pavitra <pavi.8081@gmail.com>
2023-01-22 18:39:39 +0530

    Time: 2 ms (99.65%), Space: 43.7 MB (62.38%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -4,22 +4,24 @@ class Solution {
 
         for(int i = 0; i < nums.length; i++){
             int v = nums[i];
-            if(v >= m1){
-                m3 = m2;
-                m2 = m1;
-                m1 = v;
-            }else if(v >= m2){
+            if(v >= m3){
+                m3 = v;
+            } 
+            if(v >= m2){
                 m3= m2;
                 m2= v;
-            }else if(v >= m3){
-                m3 = v;
+            } 
+            if(v >= m1){
+                m2 = m1;
+                m1 = v;
             }
             
-            if(v <= lo1){
-                lo2= lo1;
-                lo1 = v;
-            }else if(v <= lo2){
+            if(v <= lo2){
                 lo2 = v;
+            }
+            if(v <= lo1){
+                lo2 = lo1;
+                lo1 = v;
             }
         }
         return Math.max(lo1*lo2*m1, m1*m2*m3);
