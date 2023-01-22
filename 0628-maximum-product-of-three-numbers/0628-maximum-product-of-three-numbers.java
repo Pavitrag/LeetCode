53e9e41423af044f29704cd651fc3e450b27fdfd
Pavitra <pavi.8081@gmail.com>
2023-01-22 14:16:35 +0530

    Time: 13 ms (37.97%), Space: 54.3 MB (26.72%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -1,44 +1,8 @@
 class Solution {
     public int maximumProduct(int[] nums) {
-        int lo1, lo2, m3=0, m2=0, m1=0;
-        assert nums.length >= 3;
-        int a = nums[0], b = nums[1], c = nums[2];
-        if(a <= b){
-            if(b <= c){
-                m1 = c;
-                m2 = b;
-                m3 = a;
-            }else{
-                 m1 = b;
-                if(a <= c){
-                    m2 = c;
-                    m3 = a;
-                }else{
-                    m2 = a;
-                    m3 = c;
-                }
-            }
-        }else{ //a > b
-            if(c <= a){
-                m1 = a;
-                if(b < c){
-                    m3 = b;
-                    m2 = c;
-                }else{
-                    m3 = c;
-                    m2 = b;
-                }
-            }else{ // c>= a
-                m1 = c;
-                m2 = a;
-                m3 = b;
-            }
-        }
+        int lo1=Integer.MAX_VALUE, lo2=Integer.MAX_VALUE, m3=Integer.MIN_VALUE, m2=Integer.MIN_VALUE, m1=Integer.MIN_VALUE;
 
-        lo1 = m3;
-        lo2 = m2;
-
-        for(int i = 3; i < nums.length; i++){
+        for(int i = 0; i < nums.length; i++){
             int v = nums[i];
             if(v >= m1){
                 m3 = m2;
@@ -49,7 +13,9 @@ class Solution {
                 m2= v;
             }else if(v >= m3){
                 m3 = v;
-            }else if(v <= lo1){
+            }
+            
+            if(v <= lo1){
                 lo2= lo1;
                 lo1 = v;
             }else if(v <= lo2){
