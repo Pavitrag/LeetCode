0e3eecf01f7c2af7bebbcef953f2011e1356423f
Pavitra <pavi.8081@gmail.com>
2023-01-30 14:14:03 +0530

    Time: 9 ms (50.74%), Space: 47 MB (89.35%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,35 +1,23 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         //O(n.log(n))
-        Arrays.sort(intervals, Comparator.comparingInt(interval -> interval[1]));
-        record Interval(int s, int e){};
-        Stack<Interval> stack = new Stack<>();
+        Arrays.sort(intervals, Comparator.comparingInt(interval -> interval[0]));
+        //record Interval(int s, int e) {/* DO NOTHING */}
 
-        stack.push(new Interval(intervals[0][0], intervals[0][1]));
-        for(int i = 1; i < intervals.length; i++){
-            Interval curr = new Interval(intervals[i][0], intervals[i][1]);
-            while(!stack.isEmpty() && (curr.s() <= stack.peek().s())){ //current covers completely the interval on top of stack
-                stack.pop();
-            }
-            if(stack.isEmpty()){
-                stack.push(curr);
-            }else{ //stack has some elements which either are disjoint or overlap with current interval
-                if(curr.s() <= stack.peek().e()){ //overlap
-                    Interval top = stack.pop();
-                    stack.push(new Interval(Math.min(top.s(), curr.s()), Math.max(top.e(), curr.e())));
-                }else{ //disjoint
-                    stack.push(curr);
-                }
+        List<int[]> result = new ArrayList<>();
+
+        result.add(intervals[0]);
+        for (int i = 1; i < intervals.length; i++) {
+            int[] curr = intervals[i];
+            int[] prev = result.get(result.size()-1);
+            if(curr[0] <= prev[1]){
+                prev[0] = Math.min(prev[0], curr[0]);
+                prev[1] = Math.max(prev[1], curr[1]);
+            }else{
+                result.add(curr);
             }
         }
 
-        int[][] out = new int[stack.size()][2];
-
-        int size = stack.size();
-        for(int i = size-1; i >= 0; i--){
-            Interval top = stack.pop();
-            out[i] = new int[]{top.s(), top.e()};
-        }
-        return out;
-    }
+        return result.toArray(new int[result.size()][]);
+    }//:~
 }
