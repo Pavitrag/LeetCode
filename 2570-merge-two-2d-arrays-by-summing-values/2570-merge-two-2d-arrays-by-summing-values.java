eba0fa4d0af46f3dc14e040b9ac3f4df7a653b31
Pavitra <pavi.8081@gmail.com>
2024-12-16 20:30:08 +0530

    Time: 1 ms (89.64%), Space: 45.4 MB (43.62%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.java
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.java
@@ -1,32 +1,34 @@
 class Solution {
     
-    private record Pair(int id, int val){}
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+        record Pair(int id, int val){}
         int len1 = nums1.length;
         int len2 = nums2.length;
-        List<Pair> out = new ArrayList<>();
+        List<int[]> out = new ArrayList<>();
         int i = 0, j = 0;
 
-        for(; i < len1 && j < len2; /*no-op*/){
+        while(i < len1 && j < len2){
             if(nums1[i][0] < nums2[j][0]){
-                out.add(new Pair(nums1[i][0], nums1[i][1]));
+                out.add(new int[]{nums1[i][0], nums1[i][1]});
                 i++;
             }else if (nums1[i][0] > nums2[j][0]){
-                out.add(new Pair(nums2[j][0], nums2[j][1]));
+                out.add(new int[]{nums2[j][0], nums2[j][1]});
                 j++;
             }else{
-                out.add(new Pair(nums1[i][0], nums1[i][1]+nums2[j][1]));
+                out.add(new int[]{nums1[i][0], nums1[i][1]+nums2[j][1]});
                 i++; j++;
             }
         }
-        for(; i < len1; i++){
-            out.add(new Pair(nums1[i][0], nums1[i][1]));
+        while(i < len1){
+            out.add(new int[]{nums1[i][0], nums1[i][1]});
+            i++;
         }
-        for(; j < len2; j++){
-            out.add(new Pair(nums2[j][0], nums2[j][1]));
+        while(j < len2){
+            out.add(new int[]{nums2[j][0], nums2[j][1]});
+            j++;
         }
-        
-        return out.stream().map(pair -> new int[]{pair.id(), pair.val()})
-                .toArray(int[][]::new);
+        return out.toArray(new int[out.size()][2]);
+        // return out.stream().map(pair -> new int[]{pair.id(), pair.val()})
+        //         .toArray(int[][]::new);
     }
 }
